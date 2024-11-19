5aecb081eae4cc803eccce6b468baf1ee1eadb19
Daniel Thomas du Toit <92588105+dft-dutoit@users.noreply.github.com>
2024-11-19 09:49:06 +0000

    Create prep.sh
    Added ARC module requirements for lmp

---
+++ lammps-md/prep.sh
@@ -0,0 +1,20 @@
+module purge
+# Module order is important
+module load Anaconda3/2020.11
+module load foss/2020a
+module load libpng/1.6.37-GCCcore-9.3.0
+module load libjpeg-turbo/2.0.4-GCCcore-9.3.0
+module load netCDF/4.7.4-gompi-2020a
+module load GSL/2.6-GCC-9.3.0
+module load zlib/1.2.11-GCCcore-9.3.0
+module load gzip/1.10-GCCcore-9.3.0
+module load cURL/7.69.1-GCCcore-9.3.0
+module load HDF5/1.10.6-gompi-2020a
+module load tbb/2020.1-GCCcore-9.3.0
+module load PCRE2/10.34-GCCcore-9.3.0
+module load libxml2/2.9.10-GCCcore-9.3.0
+module load FFmpeg/4.2.2-GCCcore-9.3.0
+module load Voro++/0.4.6-GCCcore-9.3.0
+module load kim-api/2.1.3-foss-2020a
+module load Eigen/3.4.0-GCCcore-9.3.0
+module load ScaFaCoS/1.0.1-foss-2020a
